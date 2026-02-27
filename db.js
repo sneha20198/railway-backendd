94ae429abfe0cca9bdf8f988dafd0424e3b3ae35
sneha20198 <snehasoorupamohanty@gmail.com>
2026-02-27 16:08:11 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("Database connected successfully!");
+  }
+});
+
+module.exports = db;
